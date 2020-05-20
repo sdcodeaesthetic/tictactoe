bb972f5dc63858a049265ef06864b03eb6acbb49
sdcodeaesthetic <swarnadippramanik09@gmail.com>
2020-05-20 20:42:37 +0530

    dialog boxes added

--- gui_tictactoe.py
+++ gui_tictactoe.py
@@ -1,24 +1,43 @@
 from tkinter import *
 from tkinter.messagebox import *
+from tkinter import simpledialog
 
 class TicTacToe():
     def __init__(self, master):
         self.master = master
         self.game_board = [[], [], []]
         self.current_player = 'X'
+        self.answer = True
+        self.player1_name = 'NULL'
+        self.player2_name = 'NONE'
+        self.bottom_label = ''
         self.gui_interface()
 
     def game_button(self):
-        b = Button(self.master, padx = 1, bg = "papaya whip", width = 3, text = "", font = ('arial', 60, 'bold'), relief = "sunken", bd=10)
+        b = Button(self.master, padx = 1, bg = "navajo white", width = 3, text = "", font = ('arial', 60, 'bold'), relief = "sunken", bd=10)
         return b
 
     def click(self, row, col):
-        self.game_board[row][col].config(text = self.current_player, state = "disabled")
-        self.play_game()
-        if self.current_player == 'X':
-            self.current_player = 'O'
+        if self.answer:
+            pass
         else:
-            self.current_player = 'X'
+            self.game_board[row][col].config(text = self.current_player, state = "disabled")
+            if self.current_player == 'X':
+                self.current_player = 'O'
+                self.bottom_label.config(text = "{}'s Chance".format(self.player2_name))
+            else:
+                self.current_player = 'X'
+                self.bottom_label.config(text = "{}'s Chance".format(self.player1_name))
+            self.play_game()
+
+    def initial_gui(self):
+        self.answer = askyesno("Welcome", "Do you want to play the game as Single Player ?")
+        if self.answer == True:
+            self.player1_name = simpledialog.askstring("Player", "Please, Enter Your Name")
+            self.player2_name = 'Computer'
+        else:
+            self.player1_name = simpledialog.askstring("Player 1(X)", "Please, Enter Your Name")
+            self.player2_name = simpledialog.askstring("Player 2(O)", "Please, Enter Your Name")
 
     def gui_interface(self):
         for i in range(3):
@@ -26,6 +45,10 @@ class TicTacToe():
                 self.game_board[i].append(self.game_button())
                 self.game_board[i][j].config(command = lambda row = i, col = j: self.click(row, col))
                 self.game_board[i][j].grid(row = i,column = j)
+        self.initial_gui()
+        label = Label(self.master, text = "{}'s Chance".format(self.player1_name), font = ('arial', 20, 'bold'))
+        label.grid(row = 3, column = 0, columnspan = 3)
+        self.bottom_label = label
 
     def check_row(self):
         r1 = self.game_board[0][0]["text"] == self.game_board[0][1]["text"] == self.game_board[0][2]["text"] != ''
@@ -72,15 +95,18 @@ class TicTacToe():
             return True
 
     def reset(self):
+        self.current_player = 'X'
         for i in range(3):
             for j in range(3):
                 self.game_board[i][j]["text"] = ""
-                self.game_board[i][j]["state"] = "normal"
-        self.current_player = 'X'        
+                self.game_board[i][j]["state"] = "normal"       
 
     def play_game(self):
         if(self.check_win()):
-            print(showinfo("showinfo", "{}'s is Winner".format(self.current_player)))
+            if(self.current_player == 'X'):
+                print(showinfo("showinfo", "{} is Winner".format(self.player1_name)))
+            else:
+                print(showinfo("showinfo", "{} is Winner".format(self.player2_name)))
             self.reset()
         elif(self.check_draw()):
             print(showinfo("showinfo", "Game Drawn"))
